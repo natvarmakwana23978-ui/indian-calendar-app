1bf1dcd60dad4abf1836781738d9a66e0b671a23
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2025-12-27 19:40:23 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,14 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("org.json:json:20230227")
+
+    // --- GOOGLE ML KIT TRANSLATION LIBRARIES (OFFLINE SUPPORT) ---
+    // આ લાઈનો વિશ્વની કોઈપણ ભાષામાં ઇન્ટરનેટ વગર ભાષાંતર કરવા માટે છે
+    implementation("com.google.mlkit:translate:17.0.1")
+    implementation("com.google.android.gms:play-services-mlkit-language-id:17.0.0")
     
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
