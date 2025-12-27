544e1c92efcaa46a6db497bc2fb28667a3c16024
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2025-12-27 19:48:43 +0530

    Update MainActivity.kt

--- app/src/main/java/com/indian/calendar/MainActivity.kt
+++ app/src/main/java/com/indian/calendar/MainActivity.kt
@@ -9,7 +9,12 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,52 +24,103 @@ class MainActivity : AppCompatActivity() {
 
         val sharedPref = getSharedPreferences("CalendarPrefs", Context.MODE_PRIVATE)
 
-        // ૧. કેલેન્ડર સ્પિનર સેટઅપ
+        // ૧. કેલેન્ડર સ્પિનર
         val calSpinner: Spinner = findViewById(R.id.calendar_spinner)
-        val calOptions = arrayOf("વિક્રમ સંવત", "શક સંવત", "હિજરી", "નાનકશાહી", "ચાઈનીઝ")
-        val calKeys = arrayOf("vikram_samvat", "shaka_samvat", "hijri", "nanakshahi", "chinese")
+        val calOptions = arrayOf("વિક્રમ સંવત", "શક સંવત", "હિજરી", "નાનકશાહી", "ચાઈનીઝ", "બૌદ્ધ", "પારસી")
+        val calKeys = arrayOf("vikram_samvat", "shaka_samvat", "hijri", "nanakshahi", "chinese", "buddhist", "persian")
         
-        val calAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, calOptions)
-        calAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        calSpinner.adapter = calAdapter
+        calSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, calOptions)
 
-        // ૨. ભાષા સ્પિનર સેટઅપ
+        // ૨. ગ્લોબલ ભાષા લિસ્ટ (વિશ્વની તમામ મુખ્ય ભાષાઓ)
         val langSpinner: Spinner = findViewById(R.id.language_spinner)
-        val langOptions = arrayOf("English", "ગુજરાતી", "Hindi")
-        val langCodes = arrayOf("en", "gu", "hi")
         
-        val langAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, langOptions)
-        langAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        langSpinner.adapter = langAdapter
+        // ભાષાના નામ અને તેના ML Kit કોડની જોડી
+        val langMap = linkedMapOf(
+            "English" to TranslateLanguage.ENGLISH,
+            "ગુજરાતી" to TranslateLanguage.GUJARATI,
+            "Hindi (हिन्दी)" to TranslateLanguage.HINDI,
+            "Marathi (मराठी)" to TranslateLanguage.MARATHI,
+            "Bengali (বাংলা)" to TranslateLanguage.BENGALI,
+            "Tamil (தமிழ்)" to TranslateLanguage.TAMIL,
+            "Telugu (తెలుగు)" to TranslateLanguage.TELUGU,
+            "Kannada (ಕನ್ನಡ)" to TranslateLanguage.KANNADA,
+            "Malayalam (മലയാളം)" to TranslateLanguage.MALAYALAM,
+            "Spanish (Español)" to TranslateLanguage.SPANISH,
+            "French (Français)" to TranslateLanguage.FRENCH,
+            "German (Deutsch)" to TranslateLanguage.GERMAN,
+            "Russian (Русский)" to TranslateLanguage.RUSSIAN,
+            "Japanese (日本語)" to TranslateLanguage.JAPANESE,
+            "Chinese (中文)" to TranslateLanguage.CHINESE,
+            "Arabic (العربية)" to TranslateLanguage.ARABIC,
+            "Hebrew (עברית)" to TranslateLanguage.HEBREW,
+            "Portuguese (Português)" to TranslateLanguage.PORTUGUESE,
+            "Korean (한국어)" to TranslateLanguage.KOREAN,
+            "Italian (Italiano)" to TranslateLanguage.ITALIAN,
+            "Turkish (Türkçe)" to TranslateLanguage.TURKISH,
+            "Dutch (Nederlands)" to TranslateLanguage.DUTCH,
+            "Polish (Polski)" to TranslateLanguage.POLISH,
+            "Indonesian (Bahasa)" to TranslateLanguage.INDONESIAN,
+            "Vietnamese (Tiếng Việt)" to TranslateLanguage.VIETNAMESE,
+            "Thai (ไทย)" to TranslateLanguage.THAI
+        )
 
-        // અગાઉનું સિલેક્શન લોડ કરો
+        val langNames = langMap.keys.toTypedArray()
+        val langCodes = langMap.values.toTypedArray()
+
+        langSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, langNames)
+
+        // લોડ સેવ્ડ ડેટા
         val savedCal = sharedPref.getString("selected_key", "vikram_samvat")
-        calSpinner.setSelection(calKeys.indexOf(savedCal))
+        calSpinner.setSelection(calKeys.indexOf(savedCal).coerceAtLeast(0))
 
-        val savedLang = sharedPref.getString("selected_language", "gu")
-        langSpinner.setSelection(langCodes.indexOf(savedLang))
+        val savedLang = sharedPref.getString("selected_language", TranslateLanguage.ENGLISH)
+        langSpinner.setSelection(langCodes.indexOf(savedLang).coerceAtLeast(0))
 
-        // કેલેન્ડર બદલવા માટેનું લોજિક
+        // ઈવેન્ટ્સ
         calSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p: AdapterView<*>?, v: View?, pos: Int, id: Long) {
-                sharedPref.edit().putString("selected_key", calKeys[pos]).commit()
-                updateWidget()
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                sharedPref.edit().putString("selected_key", calKeys[pos]).apply()
+                notifyWidgetUpdate()
             }
-            override fun onNothingSelected(p: AdapterView<*>?) {}
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
         }
 
-        // ભાષા બદલવા માટેનું લોજિક
         langSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p: AdapterView<*>?, v: View?, pos: Int, id: Long) {
-                sharedPref.edit().putString("selected_language", langCodes[pos]).commit()
-                updateWidget()
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                val selectedLang = langCodes[pos]
+                sharedPref.edit().putString("selected_language", selectedLang).apply()
+                
+                // ઓફલાઇન ટ્રાન્સલેશન માટે મોડેલ ડાઉનલોડ કરો
+                prepareTranslator(selectedLang)
             }
-            override fun onNothingSelected(p: AdapterView<*>?) {}
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
         }
     }
 
-    // વિજેટને તરત જ રિફ્રેશ કરવાનું ફંક્શન
-    private fun updateWidget() {
+    private fun prepareTranslator(targetLang: String) {
+        if (targetLang == TranslateLanguage.ENGLISH) {
+            notifyWidgetUpdate()
+            return
+        }
+
+        val options = TranslatorOptions.Builder()
+            .setSourceLanguage(TranslateLanguage.ENGLISH)
+            .setTargetLanguage(targetLang)
+            .build()
+        
+        val translator = Translation.getClient(options)
+        val conditions = DownloadConditions.Builder().build() // વાઈફાઈ કે મોબાઈલ ડેટા ગમે તે ચાલશે
+        
+        translator.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+                notifyWidgetUpdate() // ડાઉનલોડ પૂરું થાય એટલે વિજેટ અપડેટ કરો
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "ഭാષા ડાઉનલોડમાં સમસ્યા છે.", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun notifyWidgetUpdate() {
         val intent = Intent(this, CalendarWidget::class.java)
         intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
         val ids = AppWidgetManager.getInstance(application).getAppWidgetIds(
@@ -74,3 +130,4 @@ class MainActivity : AppCompatActivity() {
         sendBroadcast(intent)
     }
 }
+
