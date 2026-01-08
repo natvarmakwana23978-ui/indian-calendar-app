7f7cebcdfec3e313024976bd35b820a8bcd0ab7b
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-08 21:35:05 +0530

    Update ManageCalendarActivity.kt

--- app/src/main/java/com/indian/calendar/ManageCalendarActivity.kt
+++ app/src/main/java/com/indian/calendar/ManageCalendarActivity.kt
@@ -15,7 +15,7 @@ import org.json.JSONObject
 class ManageCalendarActivity : AppCompatActivity() {
 
     // તમારી Google Apps Script URL અહીં એક જ વાર નાખો
-    private val webAppUrl = "તમારી_URL_અહીં_નાખો"
+    private val webAppUrl = "https://script.google.com/macros/s/AKfycbw7U_En4xgEayUOV3N3HwoW-GkfyOONMUxidIeZbOAxCfGJqxcXeWFvz6pnJL5nRQQ3/exec"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
