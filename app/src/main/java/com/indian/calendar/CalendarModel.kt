b73a07508e9da2d6b0c521a18c0652fb9268f757
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-13 19:36:55 +0530

    Update CalendarModel.kt

--- app/src/main/java/com/indian/calendar/CalendarModel.kt
+++ app/src/main/java/com/indian/calendar/CalendarModel.kt
@@ -3,9 +3,6 @@ package com.indian.calendar
 import com.google.gson.annotations.SerializedName
 
 data class CalendarModel(
-    @SerializedName("calendarName")
-    val name: String,
-
-    @SerializedName("creatorName")
-    val creator: String
+    @SerializedName("calendarName") val name: String,
+    @SerializedName("index") val index: Int
 )
