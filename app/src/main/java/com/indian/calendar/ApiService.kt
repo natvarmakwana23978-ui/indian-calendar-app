e1ed77d0a4349844e1d34108a21da78c04bf1cbc
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-13 12:36:59 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface ApiService {
     // આખી લિંકને બદલે માત્ર 'macros/...' વાળો ભાગ જ લખવો
-    @GET("macros/s/AKfycbwDwYZJb_nPjjqoUI28-7FNksbv6o766zWjeRhewouc0vvnNJXj8s4EkTnuWWxh4U7l/exec")
+    @GET("macros/s/AKfycbzCMHnwTK5VT8p7-IV2EvmwaJKuW48A-vUmyk2Ymzcjq7w8GB7KcyxW6DOIAecCmuwM/exec")
     fun getCalendars(): Call<List<CalendarModel>>
 }
