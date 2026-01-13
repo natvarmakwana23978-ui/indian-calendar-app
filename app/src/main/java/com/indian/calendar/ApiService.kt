cb22c8a062827ef7dfafdf0525a31b76d588dfb4
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-13 13:48:38 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -5,7 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    // Apps Script માંથી ડેટા મેળવવા માટેની મેથડ
     @GET("exec")
     fun getCalendarData(@Query("colIndex") colIndex: Int): Call<List<CalendarDayData>>
 }
