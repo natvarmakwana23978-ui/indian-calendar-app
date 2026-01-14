720fd12aa9d18d0dc5eda8845569d15411220968
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-14 13:40:10 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/indian/calendar/RetrofitClient.kt
+++ app/src/main/java/com/indian/calendar/RetrofitClient.kt
@@ -4,14 +4,14 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    // તમારી Apps Script ને Deploy કર્યા પછી મળતી લિંક અહીં 'exec' પહેલા સુધીની નાખો
-    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbzcqnaGd3uJ4-q4W0eFz7XUi0eymsSxjN1wapHDNTfNnJmjx-Yj2PECk1O7u8i9ULJy/exec/"
+    // તમારી નવી લિંક અહીં સેટ છે
+    private const val BASE_URL = "https://script.google.com/macros/s/AKfycby0axtqyL6yjPOsHjPtKYBdQtp5C6IbJADsh5QA22ADS8Z9hi5HY2y17dLUK3t0VOob/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        retrofit.create(ApiService::class.java)
+        retrofit.create(ApiService.class.java)
     }
 }
