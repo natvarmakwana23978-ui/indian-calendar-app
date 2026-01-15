825e1d89d248c903ab05ce4a7821ccdd822e9e1c
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 16:46:56 +0530

    Update CalendarRepository.kt

--- app/src/main/java/com/indian/calendar/CalendarRepository.kt
+++ app/src/main/java/com/indian/calendar/CalendarRepository.kt
@@ -1,1 +1,11 @@
+package com.indian.calendar
+
+object CalendarRepository {
+
+    var selectedCalendarIndex: Int = 0
+
+    var calendars: List<CalendarItem> = emptyList()
+
+    var calendarData: List<CalendarDayData> = emptyList()
+}
 
