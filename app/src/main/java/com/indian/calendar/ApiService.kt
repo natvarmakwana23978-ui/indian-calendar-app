960e76560ea3a64c12b5fec9776493e319e7e54f
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 10:46:38 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -3,14 +3,16 @@ package com.indian.calendar
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
-import retrofit2.http.Url
 
 interface ApiService {
-    // @Url વાપરવાથી આપણે ડાયરેક્ટ આખી લિંક મોકલી શકીશું
-    @GET("https://script.google.com/macros/s/AKfycbEBQpPWdz_7SjzjghU4IaMBIaR98LOrop8qeGhBXmlfog028zg6TfdujX0RQzehUDH/exec")
+
+    // Calendar List (Gujarati, Hindi, etc.)
+    @GET("exec")
     fun getCalendars(): Call<List<CalendarModel>>
 
-    // બીજા ફંક્શનમાં પણ આખું URL જ વાપરીએ (ફક્ત colIndex પેરામીટર સાથે)
-    @GET("https://script.google.com/macros/s/AKfycbEBQpPWdz_7SjzjghU4IaMBIaR98LOrop8qeGhBXmlfog028zg6TfdujX0RQzehUDH/exec")
-    fun getCalendarData(@Query("colIndex") colIndex: Int): Call<List<CalendarDayData>>
+    // Calendar Date-wise Data
+    @GET("exec")
+    fun getCalendarData(
+        @Query("colIndex") colIndex: Int
+    ): Call<List<CalendarDayData>>
 }
