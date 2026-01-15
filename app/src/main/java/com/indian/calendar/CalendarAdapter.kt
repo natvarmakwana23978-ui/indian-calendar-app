ec0d083db0b5b09716211d134ce21fbf19d28e79
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 13:30:33 +0530

    Update CalendarAdapter.kt

--- app/src/main/java/com/indian/calendar/CalendarAdapter.kt
+++ app/src/main/java/com/indian/calendar/CalendarAdapter.kt
@@ -6,8 +6,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-data class CalendarData(val englishDate: String, val tithi: String?, val festival: String?)
-
 class CalendarAdapter(private var data: List<CalendarData>) :
     RecyclerView.Adapter<CalendarAdapter.ViewHolder>() {
 
