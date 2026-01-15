967d8015e4127ef5dd54172b1cf3d53a1afbea73
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 16:48:00 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/indian/calendar/RetrofitClient.kt
+++ app/src/main/java/com/indian/calendar/RetrofitClient.kt
@@ -10,7 +10,7 @@ object RetrofitClient {
     private const val BASE_URL =
         "https://script.google.com/macros/s/AKfycbzsFfM_jo2P_PmCtDyccoC6KIubETZxjAnAtwLBTJRtidKIicS5cKf9l5KrMC9TDRWt/"
 
-    private val client = OkHttpClient.Builder()
+    private val okHttp = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
         .followRedirects(true)
@@ -19,7 +19,7 @@ object RetrofitClient {
     val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(client)
+            .client(okHttp)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
