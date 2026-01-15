16ea891f3b5d9b39b9edc69a965e89b915f8487a
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 16:30:01 +0530

    Update CalendarDayData.kt

--- app/src/main/java/com/indian/calendar/CalendarDayData.kt
+++ app/src/main/java/com/indian/calendar/CalendarDayData.kt
@@ -2,8 +2,6 @@ package com.indian.calendar
 
 data class CalendarDayData(
     val englishDate: String,
-    val localDate: String,
-    val weekday: String,
-    val festival: String?,
-    val category: String?
+    val localDate: String?,
+    val festival: String?
 )
