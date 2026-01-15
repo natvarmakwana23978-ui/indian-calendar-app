d0e0ae42fc2da99e89c7cd290076bcbe467afbad
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 13:29:53 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/indian/calendar/RetrofitClient.kt
+++ app/src/main/java/com/indian/calendar/RetrofitClient.kt
@@ -1,3 +1,25 @@
+package com.indian.calendar
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
+
+// Data models
+data class CalendarItem(val calendarName: String, val creatorName: String)
+data class CalendarData(val englishDate: String, val tithi: String?, val festival: String?)
+
+// Retrofit API
+interface ApiService {
+    @GET("exec")
+    suspend fun getCalendars(): List<CalendarItem>
+
+    @GET("exec")
+    suspend fun getCalendarData(@Query("colIndex") colIndex: Int): List<CalendarData>
+}
+
 object RetrofitClient {
     private const val BASE_URL = "https://script.google.com/macros/s/AKfycbzsFfM_jo2P_PmCtDyccoC6KIubETZxjAnAtwLBTJRtidKIicS5cKf9l5KrMC9TDRWt/"
 
