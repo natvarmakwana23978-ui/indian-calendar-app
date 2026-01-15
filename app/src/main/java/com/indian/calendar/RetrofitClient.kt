c2a77e4fb9683144d5f5c648aeea9078863f690c
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 16:29:20 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/indian/calendar/RetrofitClient.kt
+++ app/src/main/java/com/indian/calendar/RetrofitClient.kt
@@ -3,36 +3,23 @@ package com.indian.calendar
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
 import java.util.concurrent.TimeUnit
 
-// Data models
-data class CalendarItem(val calendarName: String, val creatorName: String)
-data class CalendarData(val englishDate: String, val tithi: String?, val festival: String?)
-
-// Retrofit API
-interface ApiService {
-    @GET("exec")
-    suspend fun getCalendars(): List<CalendarItem>
-
-    @GET("exec")
-    suspend fun getCalendarData(@Query("colIndex") colIndex: Int): List<CalendarData>
-}
-
 object RetrofitClient {
-    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbzsFfM_jo2P_PmCtDyccoC6KIubETZxjAnAtwLBTJRtidKIicS5cKf9l5KrMC9TDRWt/"
 
-    private val okHttp = OkHttpClient.Builder()
+    private const val BASE_URL =
+        "https://script.google.com/macros/s/AKfycbzsFfM_jo2P_PmCtDyccoC6KIubETZxjAnAtwLBTJRtidKIicS5cKf9l5KrMC9TDRWt/"
+
+    private val client = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
         .followRedirects(true)
         .build()
 
-    val instance: ApiService by lazy {
+    val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(okHttp)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
