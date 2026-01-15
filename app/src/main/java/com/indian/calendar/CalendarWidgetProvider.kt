7b291b8265e096cb37ea81213699c92f3c763de3
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 16:07:04 +0530

    Update CalendarWidgetProvider.kt

--- app/src/main/java/com/indian/calendar/CalendarWidgetProvider.kt
+++ app/src/main/java/com/indian/calendar/CalendarWidgetProvider.kt
@@ -1,1 +1,17 @@
+package com.indian.calendar
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class CalendarWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Later we will update widget with real data
+    }
+}
 
