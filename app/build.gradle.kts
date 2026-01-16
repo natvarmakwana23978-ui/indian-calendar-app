a4cdda4278bc5a645bbfd3be2aeb5788dc9ec5a8
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-16 12:56:54 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id 'com.android.application'
+    id 'org.jetbrains.kotlin.android'
+    id 'kotlin-parcelize' // ✅ Parcelize support
 }
 
 android {
