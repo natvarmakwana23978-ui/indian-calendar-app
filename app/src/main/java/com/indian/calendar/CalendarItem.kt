145ffaa6272270f88b28375448bbceb1ce938bf7
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-16 21:49:25 +0530

    Update CalendarItem.kt

--- app/src/main/java/com/indian/calendar/CalendarItem.kt
+++ app/src/main/java/com/indian/calendar/CalendarItem.kt
@@ -1,7 +1,7 @@
 package com.indian.calendar
 
 data class CalendarItem(
-    val calendarName: String,
-    val creatorName: String
+    val id: String = "",
+    val name: String = "",       // દા.ત. "ગુજરાતી (Gujarati)"
+    val colIndex: Int = 0        // શીટમાં આ ભાષા કયા નંબરના કોલમમાં છે તે
 )
-
