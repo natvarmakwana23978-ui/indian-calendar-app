5a8dda0fadff40e5a98f2b0f3e6754bf0cc30344
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-19 13:43:46 +0530

    Update MonthPagerAdapter.kt

--- app/src/main/java/com/indian/calendar/MonthPagerAdapter.kt
+++ app/src/main/java/com/indian/calendar/MonthPagerAdapter.kt
@@ -3,7 +3,7 @@ package com.indian.calendar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.indian.calendar.model.CalendarDayData
+// 'model' ઈમ્પોર્ટ હટાવી દીધો છે કારણ કે હવે બધું એક જ પેકેજમાં છે
 
 class MonthPagerAdapter(
     activity: FragmentActivity,
@@ -13,7 +13,8 @@ class MonthPagerAdapter(
     override fun getItemCount(): Int = dayList.size
 
     override fun createFragment(position: Int): Fragment {
-        // અહીં હવે newInstance ભૂલ નહીં આપે
+        // હવે CalendarDayFragment અને CalendarDayData બંને એક જ પેકેજમાં હોવાથી
+        // અહીં કોઈ 'Unresolved reference' એરર આવશે નહીં.
         return CalendarDayFragment.newInstance(dayList[position])
     }
 }
