cedf4002267efe618b9189fe05331a0c7c7e4f36
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-19 21:28:22 +0530

    Update CalendarItem.kt

--- app/src/main/java/com/indian/calendar/CalendarItem.kt
+++ app/src/main/java/com/indian/calendar/CalendarItem.kt
@@ -3,6 +3,6 @@ package com.indian.calendar
 import com.google.gson.annotations.SerializedName
 
 data class CalendarItem(
-    @SerializedName("id") val id: String = "",
-    @SerializedName("name") val name: String = ""
+    @SerializedName("id") val id: String? = null,
+    @SerializedName("name") val name: String? = null
 )
