aaaddfb0f592b443e6d9217fdba92877c15d8b2f
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-19 16:49:56 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/indian/calendar/RetrofitClient.kt
+++ app/src/main/java/com/indian/calendar/RetrofitClient.kt
@@ -4,8 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    // તમારી ગૂગલ શીટ વેબ એપની સાચી લિંક અહીં મુકવી
-    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbzpzJAnAzBSb6Y0Hen2e8jg81Bp8DB7zOlWyAf9lKn5PmqiduqPyk7hT95-2nXpWA6q/exec" 
+    // BASE_URL હંમેશા સ્લેશ (/) થી પૂરી થવી જોઈએ અને તેમાં 'exec' ન હોવું જોઈએ
+    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbzpzJAnAzBSb6Y0Hen2e8jg81Bp8DB7zOlWyAf9lKn5PmqiduqPyk7hT95-2nXpWA6q/" 
 
     val api: ApiService by lazy {
         Retrofit.Builder()
