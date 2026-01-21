e8353da18a691692ab9e9dc08e3c68c362c55bea
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-21 13:10:04 +0530

    Update CalendarDayData.kt

--- app/src/main/java/com/indian/calendar/CalendarDayData.kt
+++ app/src/main/java/com/indian/calendar/CalendarDayData.kt
@@ -6,13 +6,12 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CalendarDayData(
-    // ગૂગલ સ્ક્રિપ્ટ આ ૩ નામથી જ ડેટા મોકલે છે [cite: 2026-01-21]
     @SerializedName("ENGLISH") 
-    val date: String = "",
+    val Date: String = "", // 'd' ને બદલે 'D' [cite: 2026-01-21]
     
     @SerializedName("local_date") 
-    val localDate: String = "", // આ એક જ લાઈન બધી ભાષાઓ (ગુજરાતી, હિન્દી વગેરે) માટે કામ કરશે [cite: 2026-01-21]
+    val Gujarati: String = "", // 'localDate' ને બદલે 'Gujarati' [cite: 2026-01-21]
     
     @SerializedName("Alert") 
-    val alert: String = ""
+    val Alert: String = ""
 ) : Parcelable
