9bd28a8c2f8708ffeffa74e7caab673baf2cb35c
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-21 21:03:51 +0530

    Update CalendarViewActivity.kt

--- app/src/main/java/com/indian/calendar/CalendarViewActivity.kt
+++ app/src/main/java/com/indian/calendar/CalendarViewActivity.kt
@@ -1,9 +1,32 @@
-// ... બાકીનો કોડ ઉપર મુજબ ...
+package com.indian.calendar
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CalendarViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar_view)
+        val recyclerView = findViewById<RecyclerView>(R.id.calendarRecyclerView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        recyclerView.layoutManager = GridLayoutManager(this, 7)
+
+        val colIndex = intent.getIntExtra("COL_INDEX", 1)
+        progressBar.visibility = View.VISIBLE
+        RetrofitClient.api.getCalendarData(colIndex = colIndex).enqueue(object : Callback<List<CalendarDayData>> {
             override fun onResponse(call: Call<List<CalendarDayData>>, response: Response<List<CalendarDayData>>) {
                 progressBar.visibility = View.GONE
                 if (response.isSuccessful) {
-                    val daysList = response.body() ?: emptyList()
-                    // અહીં નામ 'CalendarAdapter' હોવું જોઈએ [cite: 2026-01-21]
-                    recyclerView.adapter = CalendarAdapter(daysList) 
+                    recyclerView.adapter = CalendarAdapter(response.body() ?: emptyList())
                 }
-// ... બાકીનો કોડ ઉપર મુજબ ...
+            }
+            override fun onFailure(call: Call<List<CalendarDayData>>, t: Throwable) { progressBar.visibility = View.GONE }
+        })
+    }
+}
