f5a6aaf33ae010931acc0a363f435cfcdc0f014f
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-27 13:50:24 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -1,22 +1,25 @@
 package com.indian.calendar
 
+import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    // બધી ઉપલબ્ધ શીટ્સ (કેલેન્ડર) ના નામ મેળવવા માટે
     @GET("exec")
     fun getCalendarList(): Call<CalendarListResponse>
 
-    // સિલેક્ટ કરેલા કેલેન્ડરનો ડેટા મેળવવા માટે (દા.ત. calendar=calendarfinaldata)
     @GET("exec")
     fun getCalendarData(
         @Query("calendar") calendarName: String
-    ): Call<List<Map<String, String>>>
+    ): Call<List<JsonObject>>
 }
 
-// સ્ક્રિપ્ટ જે લિસ્ટ મોકલે છે તેના માટેનું મોડલ
 data class CalendarListResponse(
     val available_calendars: List<String>
+)
+
+data class CalendarDayData(
+    val englishDate: String,
+    val allData: JsonObject
 )
