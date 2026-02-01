8e34bda61cdd55d9ac76b424c9004d7d6a5371f7
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-01 17:22:00 +0530

    Update CalendarPagerAdapter.kt

--- app/src/main/java/com/indian/calendar/CalendarPagerAdapter.kt
+++ app/src/main/java/com/indian/calendar/CalendarPagerAdapter.kt
@@ -5,14 +5,8 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.gson.JsonObject
 
-class CalendarPagerAdapter(
-    fa: FragmentActivity,
-    private val allData: List<JsonObject>,
-    private val lang: String
-) : FragmentStateAdapter(fa) {
-
+class CalendarPagerAdapter(fa: FragmentActivity, private val allData: List<JsonObject>, private val lang: String) : FragmentStateAdapter(fa) {
     override fun getItemCount(): Int = 12
-
     override fun createFragment(position: Int): Fragment {
         val monthNum = position + 1
         val monthData = allData.filter { 
