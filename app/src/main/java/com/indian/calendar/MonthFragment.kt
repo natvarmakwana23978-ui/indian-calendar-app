d9786b35eda197440a25ed5bcaf86b8f34cc346f
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-01 16:31:39 +0530

    Update MonthFragment.kt

--- app/src/main/java/com/indian/calendar/MonthFragment.kt
+++ app/src/main/java/com/indian/calendar/MonthFragment.kt
@@ -12,32 +12,29 @@ import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
 
 class MonthFragment : Fragment() {
-
     companion object {
         fun newInstance(monthData: List<JsonObject>, lang: String): MonthFragment {
-            val fragment = MonthFragment()
-            val args = Bundle()
-            args.putString("MONTH_DATA", Gson().toJson(monthData))
-            args.putString("LANG", lang)
-            fragment.arguments = args
-            return fragment
+            val f = MonthFragment()
+            val b = Bundle()
+            b.putString("DATA", Gson().toJson(monthData))
+            b.putString("LANG", lang)
+            f.arguments = b
+            return f
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_month, container, false)
-        val rv = view.findViewById<RecyclerView>(R.id.rvMonthCalendar)
+        val v = inflater.inflate(R.layout.fragment_month, container, false)
+        val rv = v.findViewById<RecyclerView>(R.id.rvMonthCalendar)
         
-        val jsonData = arguments?.getString("MONTH_DATA")
-        val lang = arguments?.getString("LANG") ?: "ગુજરાતી (Gujarati)"
-        
+        val jsonData = arguments?.getString("DATA")
+        val lang = arguments?.getString("LANG") ?: ""
         val dataList: List<JsonObject> = Gson().fromJson(jsonData, object : TypeToken<List<JsonObject>>() {}.type)
+        
         val finalItems = dataList.map { CalendarDayData(it.get("ENGLISH").asString, it) }
 
         rv.layoutManager = GridLayoutManager(context, 7)
         rv.adapter = CalendarAdapter(finalItems, lang)
-        
-        return view
+        return v
     }
 }
-
