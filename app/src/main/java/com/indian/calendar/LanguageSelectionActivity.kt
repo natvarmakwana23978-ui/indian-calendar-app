f9ad0eba794ae468f9b5f5a00039eb457892b088
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-08 19:49:17 +0530

    Update LanguageSelectionActivity.kt

--- app/src/main/java/com/indian/calendar/LanguageSelectionActivity.kt
+++ app/src/main/java/com/indian/calendar/LanguageSelectionActivity.kt
@@ -3,7 +3,8 @@ package com.indian.calendar
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.indian.calendar.databinding.ActivityCalendarSelectionBinding // XML મુજબ નામ
+// જો ફાઈલનું નામ activity_calendar_selection.xml હોય, તો બાઈન્ડિંગ આ જ રહેશે:
+import com.indian.calendar.databinding.ActivityCalendarSelectionBinding 
 
 class LanguageSelectionActivity : AppCompatActivity() {
 
@@ -11,11 +12,15 @@ class LanguageSelectionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // અહીં બાઈન્ડિંગ સેટ કરીએ છીએ
         binding = ActivityCalendarSelectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnNext.setOnClickListener {
+            // ખાતરી કરજો કે તમારા XML માં Spinner નું ID 'languageSpinner' જ હોય
             val selectedLang = binding.languageSpinner.selectedItem.toString()
+            
             val intent = Intent(this, CalendarSelectionActivity::class.java)
             intent.putExtra("SELECTED_LANG", selectedLang)
             startActivity(intent)
