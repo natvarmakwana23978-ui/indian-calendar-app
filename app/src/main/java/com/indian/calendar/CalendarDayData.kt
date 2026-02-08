84b10e942f027730b391dbe6766f58f9d59f971e
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-08 13:53:53 +0530

    Update CalendarDayData.kt

--- app/src/main/java/com/indian/calendar/CalendarDayData.kt
+++ app/src/main/java/com/indian/calendar/CalendarDayData.kt
@@ -3,12 +3,8 @@ package com.indian.calendar
 import com.google.gson.JsonObject
 
 data class CalendarDayData(
-    val englishDate: String,
-    val allData: JsonObject,
-    var colorCode: Int = 0, // 0=Normal, 1=Red(Weekend), 2=Orange, 3=Green, 4=Blue
-    val isEmpty: Boolean = false,
-    
-    // New fields for widget
-    val isWeekend: Boolean = false,
-    val hasReminders: Boolean = false
+    val date: String,          // તારીખ
+    val color_code: Int,       // ૧ એટલે રજા
+    val details: JsonObject? = null, // ૨૭ કેલેન્ડરની વિગતો
+    val isSunday: Boolean = false
 )
