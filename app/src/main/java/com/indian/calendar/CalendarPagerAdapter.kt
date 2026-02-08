4db804864e613df7d1cf268e90ab428a8eb0fc6b
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-08 19:37:11 +0530

    Update CalendarPagerAdapter.kt

--- app/src/main/java/com/indian/calendar/CalendarPagerAdapter.kt
+++ app/src/main/java/com/indian/calendar/CalendarPagerAdapter.kt
@@ -3,21 +3,13 @@ package com.indian.calendar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.gson.Gson
-import com.google.gson.JsonObject
 
-class CalendarPagerAdapter(
-    fa: FragmentActivity, 
-    private val fullJsonString: String, // આખી JSON String લો
-    private val lang: String, 
-    private val calIndex: Int
-) : FragmentStateAdapter(fa) {
-
-    override fun getItemCount(): Int = 12
+class CalendarPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int = 12 // ૧૨ મહિના માટે
 
     override fun createFragment(position: Int): Fragment {
-        // આખું JSON સ્ટ્રિંગ તરીકે જ Fragment માં મોકલી દો
-        // ત્યાં Fragment ની અંદર આપણે મહિનો અને કેલેન્ડર ઇન્ડેક્સ મુજબ ડેટા ફિલ્ટર કરીશું
-        return MonthFragment.newInstance(fullJsonString, lang, calIndex, position + 1)
+        val fragment = MonthFragment()
+        // આ રીતે ડેટા પાસ કરવાથી 'newInstance' ની જરૂર નહીં પડે
+        return fragment
     }
 }
