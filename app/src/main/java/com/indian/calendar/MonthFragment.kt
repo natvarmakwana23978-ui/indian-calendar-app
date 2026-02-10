f869d89c98967a8d53631e0f57f67f1586e5ddec
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-10 21:11:45 +0530

    Update MonthFragment.kt

--- app/src/main/java/com/indian/calendar/MonthFragment.kt
+++ app/src/main/java/com/indian/calendar/MonthFragment.kt
@@ -6,17 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import com.indian.calendar.databinding.FragmentMonthBinding
+import com.smart.reminder.databinding.FragmentMonthBinding
 
 class MonthFragment : Fragment() {
 
     private var _binding: FragmentMonthBinding? = null
     private val binding get() = _binding!!
-    
-    private var monthData: List<CalendarDayData> = emptyList()
-    private var language: String = "Gujarati"
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentMonthBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -24,9 +24,10 @@ class MonthFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         
-        // તમારી XML માં ID 'rvMonthCalendar' છે એમ માનીને:
-        binding.rvMonthCalendar.layoutManager = GridLayoutManager(context, 7)
-        binding.rvMonthCalendar.adapter = MonthAdapter(monthData, language)
+        // ગ્રીડ લેઆઉટ (૭ દિવસ માટે)
+        binding.recyclerViewMonth.layoutManager = GridLayoutManager(context, 7)
+        
+        // અહીં તમારા ડેટા લોડ કરવાનું લોજિક આવશે
     }
 
     override fun onDestroyView() {
